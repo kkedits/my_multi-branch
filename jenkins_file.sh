04b23866a3a056da77199b0b0f0b868a12bc8cad
kkedits <venkatakishorevusa@gmail.com>
2025-01-29 09:07:03 +0530

    Create jenkins_file.sh

---
+++ jenkins_file.sh
@@ -0,0 +1,15 @@
+pipeline{
+    agent any
+    stages{
+        stage("code"){
+            steps{
+                echo "This is code stage"
+            }
+        }
+        stage('Build'){
+            steps{
+                echo "This is Build Stage"
+            }
+        }
+    }
+}
